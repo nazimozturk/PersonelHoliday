63e97e2c08e4f4d749505edc50037ed63cabcf2e
Nazım ÖZTÜRK <Nazm.OZTURK@hotmail.com.tr>
2022-05-29 09:07:01 +0300

    Add project files.

---
+++ Controllers/APIController/APIController.cs
@@ -0,0 +1,49 @@
+﻿using Data.Model;
+using Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PersonelHoliday.Controllers.APIController
+{
+    [Route("home/api")]
+    [ApiController]
+    public class APIController : ControllerBase
+    {
+        private readonly  PersonelRepository _personelReposiroty;
+
+         public APIController(PersonelRepository personelRepository)
+        {
+            _personelReposiroty = personelRepository;
+        }
+        
+        [HttpGet("Personel")]
+        public ActionResult Get()
+        {
+            List<PersonelModel> returnModel = _personelReposiroty.GetPersonelList();
+            
+            return Ok(returnModel);
+        }
+
+        // POST api/<APIController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<APIController>/5
+        [HttpPut("Update")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        //// DELETE api/<APIController>/5
+        //[HttpDelete("DeletePersonel/{id}")]
+        //public ActionResult Delete(int id)
+        //{
+        //    List<PersonelModel> returnModel = _personelReposiroty.Delete(id);
+
+        //    return Ok(returnModel);
+        //}
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonelHoliday.Models;
+using System.Diagnostics;
+
+namespace PersonelHoliday.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
+        public IActionResult Permit()
+        {
+            return View();
+        }
+
+        public IActionResult TestPage()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
