4e41d772fd54e6a6a7f1e8d7c3f6f7259bdfbfd8
Nazım ÖZTÜRK <Nazm.OZTURK@hotmail.com.tr>
2022-05-29 14:05:33 +0300

    20220509 - V2

--- wwwroot/Resources/myScripts/Permit.js
+++ wwwroot/Resources/myScripts/Permit.js
@@ -1,38 +1,38 @@
 ﻿
 $(document).ready(function () {
-	$("#table").bootstrapTable({
-		url: "api/Personel",
-		pagination: true,
-		search: true,
-		columns: [
-			{
-				field: "id",
-				title: "Personel ID",
-			},
-			{
-				field: "firstName",
-				title: "Personel Adı",
-			},
-			{
-				field: "lastName",
-				title: "Personel Soyadı",
-			},
-			{
-				field: "email",
-				title: "E-Posta",
-			},
-			{
-				field: "allowedPermitCount",
-				title: "Kullanılan İzin",
-			},
-			{
-				field: "activeStatus",
-				title: "Durum",
-			},
-			{
-				field: "totalPermitCount",
-				title: "Toplam İzin",
-			},
-		]
-	});
+    $("#table").bootstrapTable({
+        url: "api/Personel",
+        pagination: true,
+        search: true,
+        columns: [
+            {
+                field: "id",
+                title: "Personel ID",
+            },
+            {
+                field: "firstName",
+                title: "Personel Adı",
+            },
+            {
+                field: "lastName",
+                title: "Personel Soyadı",
+            },
+            {
+                field: "email",
+                title: "E-Posta",
+            },
+            {
+                field: "allowedPermitCount",
+                title: "Kullanılan İzin",
+            },
+            {
+                field: "activeStatus",
+                title: "Durum",
+            },
+            {
+                field: "totalPermitCount",
+                title: "Toplam İzin",
+            },
+        ]
+    });
 })
